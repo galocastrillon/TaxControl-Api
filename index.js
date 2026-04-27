2deb360cb8d90de4b39b830d27795fcb416211c0
galocastrillon <trader@castrillon.net>
2026-04-27 14:31:58 -0500

    Update express.json and express.urlencoded limits
    Increase JSON and URL-encoded body size limits.

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ dotenv.config();
 // 2️⃣ Crear app
 const app = express();
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // 3️⃣ Crear pool MariaDB (GLOBAL)
 const pool = mysql.createPool({
